cadcd6d339d2fe5f3071e55a6462ae06ed0afb17
nodadragon <edwardne@oregonstate.edu>
2017-10-10 18:59:19 -0700

    Made the output file work for the result of findK

--- Group Assignment 1/select.cpp
+++ Group Assignment 1/select.cpp
@@ -2,8 +2,6 @@
 #include<fstream>
 #include<string>
 #include <sstream>
-#include <byteswap.h>
-
 using namespace std;
 
 int binarySearch(int * values, int val, int beginning, int ending);
@@ -73,8 +71,6 @@ void truncateL(struct datFile* dats, int newBeginning, int newEnd, int subtract)
                   }
             }*/
       }
-
-
 }
 //for step 1, truncate if k> length
 void truncateK(struct datFile* dats, int k){
@@ -137,7 +133,7 @@ int findK(struct datFile* dats, int k){
                   }
                   dats[i].L = dats[i].middlePoint - dats[i].beginning;
                   cout << "Less than: " << dats[i].L << endl;
-                  if(k < dats[i].L){ //truncate off uppe rhalf. just use bottom half.
+                  if(k < dats[i].L){ //truncate off upper half. just use bottom half.
                         truncateL(dats, dats[i].beginning, dats[i].middlePoint, 1);
 
                   }else{//truncate off lower half, so just use top half
@@ -149,41 +145,6 @@ int findK(struct datFile* dats, int k){
       }
 
 
-}
-
-
-/*
-//merging two sorted arrays into one sorted arrays
-void merge(int arr1[], int arr2[], int arr3[]){
-  int x = sizeof(arr1)/sizeof(arr1[0]);
-  int y = sizeof(arr2)/sizeof(arr2[0]);
-  int i = 0, j = 0, k = 0;
-
-  //traverse both arrays
-  while(i < x && j < y){
-    if(arr1[i] < arr2[j])           //Check if the current element in the first array
-      arr3[k++] = arr1[i++];        //is smaller than the current element in second array
-    else                            //Store which one is smaller
-      arr3[k++] = arr2[j++];
-  }
-
-  while(i < x)
-    arr3[k++] = arr1[i++];
-
-  while(j < y)
-    arr3[k++] = arr2[j++];
-}*/
-
-int select(int m, int n, int k)
-{
-    int result= 1000;//just a placeholder to test file output
-
-    //Read the array from binary file.
-    //Put them into m arrays consisting of n elements
-    //Use Merge function in MergeSort to Merge all m arrays
-    //Find k with for loop
-
-    return result;
 }
 
 int main()
@@ -257,7 +218,8 @@ int main()
 
    }
 
- cout << "ANSWER: " <<  findK(dats, dats->k) << endl;;
+    result = findK(dats, dats->k);
+    cout << "ANSWER: " << result << endl;
 
     //store m n and k retrieved from input files
     m = arr[0];
@@ -268,7 +230,6 @@ int main()
     cout << "n is: " << arr[1] << "\n";
     cout << "k is: " << arr[2] << "\n";
 
-    result = select(m,n,k);
     cout << k << "th smallest element is: " << result << endl;
 
     //print result of select() to output file
