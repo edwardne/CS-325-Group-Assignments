6e9e5c602b90fa1d540e40737c8172c92b2ee6c1
lukmand <lukmand@users.noreply.github.com>
2017-10-24 22:35:07 -0700

    Update vankin.cpp

--- CS-325-Group-Assignments-master/Group Assignment 2/vankin.cpp
+++ CS-325-Group-Assignments-master/Group Assignment 2/vankin.cpp
@@ -67,7 +67,7 @@ int main()
 
     for(int j = n-1; j>=0; j--)
     {
-        VMile[n][j] = 0;
+        VMile[n][j] = 0;				//dont think u need this since the array is already emptied above
         for(int i = n-1; i>=0; i--)
         {
 
